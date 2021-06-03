5c715303b975f430db1274d21ecdcfca5131957f
produce101japan2 <test>
2021-06-04 07:22:57 +0900

    to final

--- tools/get_rank2.sh
+++ tools/get_rank2.sh
@@ -2,7 +2,7 @@
 while read line
 do
   file=`echo $line | awk -F "," '{print $1}'`
-  rank=`curl -s https://produce101.jp/profile/data2021.php?id=${file} | awk 'NR==5'`
+  rank=`curl -s https://produce101.jp/profile/data2021.php?id=${file} | awk 'NR==8'`
   if [ "${rank}" != "NULL" ]; then
     echo "${file},${rank}"
   fi
